6106a308d7195109dd5e3303d85aea78f3d23411
MBODM <webmaster@mbodm.com>
2023-06-10 16:56:25 +0200

    dev...

--- Rust/cdis/src/display.rs
+++ Rust/cdis/src/display.rs
@@ -1,94 +1,121 @@
 use ddc::Ddc;
 use ddc_winapi::Monitor;
-use std::usize;
 
-static mut MONITORS: Option<Vec<Monitor>> = None;
+#[derive(Debug)]
+pub struct DisplayControl {
+    is_initialized: bool,
+    available_monitors: Vec<Monitor>,
+}
 
-pub fn init() -> Result<(), String> {
-    let monitors = Monitor::enumerate().map_err(|_| "Could not enumerate displays.")?;
-    if monitors.is_empty() {
-        return Err(String::from("No display(s) found."));
+impl DisplayControl {
+    pub fn new() -> Self {
+        Self {
+            is_initialized: false,
+            available_monitors: vec![],
+        }
     }
-    unsafe {
-        MONITORS = Some(monitors);
+
+    pub fn init(&mut self) -> Result<(), String> {
+        if !self.is_initialized {
+            self.is_initialized = true;
+            self.available_monitors = Monitor::enumerate()
+                .map_err(|_| "Underlying DDC Monitor API failed while enumerating monitors.")?;
+            if self.available_monitors.is_empty() {
+                return Err(String::from("No connected display(s) found."));
+            }
+        }
+        Ok(())
     }
-    Ok(())
-}
 
-pub fn has_multiple_displays() -> Result<bool, String> {
-    let monitors = get_monitors()?;
-    let has_multiple_displays = monitors.len() > 1;
-    Ok(has_multiple_displays)
-}
-
-pub fn get_displays_names() -> Result<Vec<String>, String> {
-    let monitors = get_monitors()?;
-    let descriptions = monitors
-        .iter()
-        .enumerate()
-        .map(|(index, monitor)| format!("[{}] {}", index, monitor.description()))
-        .collect::<Vec<_>>();
-    Ok(descriptions)
-}
-
-pub fn get_display_name_by_id(display_id: u8) -> Result<String, String> {
-    let monitor = get_monitor_by_id(display_id)?;
-    let description = monitor.description();
-    Ok(description)
-}
-
-pub fn display_supports_vcp60(display_id: u8) -> Result<bool, String> {
-    let capabilities = get_display_capabilities(display_id)?;
-    let is_supported = capabilities.contains("60(");
-    Ok(is_supported)
-}
-
-pub fn set_vcp60_value(display_id: u8, value: u16) -> Result<(), String> {
-    let mut monitor = get_monitor_by_id(display_id)?;
-    monitor
-        .set_vcp_feature(0x60, value)
-        .map_err(|_| "Could not set DDC VCP 60 value.")?;
-    Ok(())
-}
-
-pub fn get_vcp60_value(display_id: u8) -> Result<u16, String> {
-    let monitor = get_monitor_by_id(display_id)?;
-    let value = monitor
-        .get_vcp_feature(0x06)
-        .map_err(|_| "Could not get DDC VCP 60 value.")?;
-    let vcp60_value = value.value();
-    Ok(vcp60_value)
-}
-
-pub fn get_vcp60_value_as_string(display_id: u8) -> Result<String, String> {
-    let vcp60_value = get_vcp60_value(display_id)?;
-    let s = format!("{:02X}", vcp60_value);
-    Ok(s)
-}
-
-fn get_monitors() -> Result<Vec<Monitor>, String> {
-    match unsafe { MONITORS } {
-        Some(monitors) => Ok(monitors),
-        None => Err(String::from("Make sure you called the init() function.")),
+    pub fn get_display_list(&self) -> Result<Vec<String>, String> {
+        self.check_init()?;
+        let displays = self
+            .available_monitors
+            .iter()
+            .enumerate()
+            .map(|(index, monitor)| format!("[{}] {}", index, monitor.description()))
+            .collect::<Vec<_>>();
+        Ok(displays)
     }
-}
 
-fn get_monitor_by_id(display_id: u8) -> Result<&'static mut Monitor, String> {
-    let monitors = get_monitors()?;
-    let index = usize::from(display_id);
-    if index >= monitors.len() {
-        return Err(String::from("Display with given ID not exists."));
+    pub fn has_multiple_displays(&self) -> Result<bool, String> {
+        self.check_init()?;
+        Ok(self.available_monitors.len() > 1)
     }
-    let monitor = &mut monitors[index];
-    Ok(monitor)
-}
 
-fn get_display_capabilities(display_id: u8) -> Result<String, String> {
-    let monitor = get_monitor_by_id(display_id)?;
-    let bytes = monitor
-        .capabilities_string()
-        .map_err(|_| "Could not determine the display´s DDC capabilities.")?;
-    let capabilities = String::from_utf8(bytes)
-        .map_err(|_| "Could not convert the displays´s DDC capabilities to string.")?;
-    Ok(capabilities)
+    pub fn display_id_exists(&self, display_id: u8) -> Result<bool, String> {
+        self.check_init()?;
+        let index = usize::from(display_id);
+        Ok(index < self.available_monitors.len())
+    }
+
+    pub fn get_display_description(&self, display_id: u8) -> Result<String, String> {
+        self.check_init()?;
+        let index = self.check_display_id(display_id)?;
+        let description = self.available_monitors[index].description();
+        Ok(description)
+    }
+
+    pub fn get_display_capabilities(&mut self, display_id: u8) -> Result<String, String> {
+        self.check_init()?;
+        let index = self.check_display_id(display_id)?;
+        let bytes = self.available_monitors[index]
+            .capabilities_string()
+            .map_err(|_| "Could not determine the display´s DDC capabilities.")?;
+        let capabilities = String::from_utf8(bytes)
+            .map_err(|_| "Could not convert the displays´s DDC capabilities to string.")?;
+        Ok(capabilities)
+    }
+
+    pub fn display_supports_vcp60(&mut self, display_id: u8) -> Result<bool, String> {
+        let supports60 = self.check_vcp60_support(display_id, false)?;
+        Ok(supports60)
+    }
+
+    pub fn get_vcp60_value(&mut self, display_id: u8) -> Result<u16, String> {
+        self.check_init()?;
+        let index = self.check_display_id(display_id)?;
+        self.check_vcp60_support(display_id, true)?;
+        let vcp_value = self.available_monitors[index]
+            .get_vcp_feature(0x60)
+            .map_err(|_| "Could not get DDC VCP 60 value.")?;
+        let value = vcp_value.value();
+        Ok(value)
+    }
+
+    pub fn set_vcp60_value(&mut self, display_id: u8, value: u16) -> Result<(), String> {
+        self.check_init()?;
+        let index = self.check_display_id(display_id)?;
+        self.check_vcp60_support(display_id, true)?;
+        self.available_monitors[index]
+            .set_vcp_feature(0x60, value)
+            .map_err(|_| "Could not set DDC VCP 60 value.")?;
+        Ok(())
+    }
+
+    fn check_init(&self) -> Result<(), String> {
+        match self.is_initialized {
+            true => Ok(()),
+            false => Err(String::from(
+                "Not initialized (please call 'DisplayControl::init()' first).",
+            )),
+        }
+    }
+
+    fn check_display_id(&self, display_id: u8) -> Result<usize, String> {
+        let index = usize::from(display_id);
+        if index >= self.available_monitors.len() {
+            return Err(String::from("Display with given ID not exists."));
+        }
+        Ok(index)
+    }
+
+    fn check_vcp60_support(&mut self, display_id: u8, show_error: bool) -> Result<bool, String> {
+        let capabilities = self.get_display_capabilities(display_id)?;
+        let supports60 = capabilities.contains("60(");
+        if !supports60 && show_error {
+            return Err(String::from("Display with given ID has no VCP 60 support."));
+        }
+        Ok(supports60)
+    }
 }
--- Rust/cdis/src/main.rs
+++ Rust/cdis/src/main.rs
@@ -1,3 +1,4 @@
+use display::DisplayControl;
 use std::process::ExitCode;
 
 mod display;
@@ -7,152 +8,52 @@ pub const APP_TITLE: &str = "ChangeDisplayInputSource";
 pub const RELEASE_DATE: &str = "05/2023";
 
 fn run() -> Result<bool, String> {
-    display::init()?;
-    let display_names = display::get_displays_names()?;
-    for display_name in display_names {
-        println!("{}", display_name);
+    let mut dc = DisplayControl::new();
+    dc.init()?;
+    println!("Detected displays:");
+    let list = dc.get_display_list()?;
+    for l in list {
+        println!("  {}", l);
     }
+    let multi = dc.has_multiple_displays()?;
+    println!(
+        "Has multiple displays: {}",
+        if multi { "Yes" } else { "No" }
+    );
+    let exists = dc.display_id_exists(0)?;
+    println!(
+        "Display with ID=0 exists: {}",
+        if exists { "Yes" } else { "No" }
+    );
+    let desc = dc.get_display_description(0)?;
+    println!("Description of display with ID=0: {}", desc);
+    println!("Capabilities of display with ID=0:");
+    let caps = dc.get_display_capabilities(0)?;
+    println!("  {}", caps);
+    let supports = dc.display_supports_vcp60(0)?;
+    println!(
+        "Display with ID=0 has VCP 60 support: {}",
+        if supports { "Yes" } else { "No" }
+    );
+    let value = dc.get_vcp60_value(0)?;
+    println!("VCP 60 value of display with ID=0: {:02X}", value);
     Ok(false)
 }
 
 fn main() -> ExitCode {
     print::show_title();
     // I know i could also return Result<> from main() function, since version 1.26 of Rust.
-    // But this will be more clear to me in the future, since i just code in Rust at times.
+    // But this will be more clear to me in the future, since i code in Rust just at times.
     match run() {
         Ok(b) => {
             if b {
-                print!("Have a nice day.");
+                println!("Have a nice day.");
             }
             ExitCode::SUCCESS
         }
         Err(s) => {
-            print!("Error: {}", s);
+            println!("Error: {}", s);
             ExitCode::FAILURE
         }
     }
 }
-
-/*
-
-
-
-// display::get_detected_displays(&monitors)?
-
-
-
-
-// if !display::print_detected_displays(&monitors) {
-//     return ExitCode::FAILURE;
-// }
-
-// if display::is_one_display_only(monitors)
-// {
-
-// }
-
-// return ExitCode::SUCCESS;
-
-// let params = env::args().collect::<Vec<String>>();
-// let params_count = env::args().count();
-// if params_count == 1 {
-//     //print::show_usage();
-//     //return ExitCode::SUCCESS;
-// }
-
-// let prim = &mut monitors[0];
-// //prim.set_vcp_feature(0, 0);
-
-// match display::set_vcp_60_code(prim, 0x03) {
-//     Ok(_) => println!("hurra"),
-//     Err(e) => eprintln!("{:?}", e),
-// }
-// return ExitCode::SUCCESS;
-//
-
-
-
-
-
-
-
-
-
-let prim = &mut monitors[0];
-if monitors.is_empty() {
-    println!("Error: Could not found any connected displays.");
-    return ExitCode::FAILURE;
-}
-// Scan
-if params_count == 2 && params[1] == "/scan" {
-    println!("Count of detected displays:       {}", monitors.len());
-
-    // Todo: if 1 sonst error
-    println!("Primary display is:               Detected display #1");
-
-    // Description
-    let desc = monitors[0].description();
-    println!("Display description:              {}", desc);
-    // Capabilities
-    let caps_raw = monitors[0]
-        .capabilities_string()
-        .expect("Error: Could not determine DDC capabilities.");
-    let caps = String::from_utf8(caps_raw)
-        .expect("Error: Could not convert determined capabilities to string.");
-    //println!("Display capabilities:");
-    //println!("{}", caps);
-    //println!();
-    // VCP 60 support
-    print!("Display has VCP 60 support:       ");
-    if !caps.contains("60(") {
-        println!("No");
-        println!();
-        println!("Error: Display not supports VCP 60 feature.");
-        return ExitCode::FAILURE;
-    }
-    println!("Yes");
-    // VCP 60 supported values
-    print!("Supported VCP 60 feature codes:   ");
-    let parts1 = caps.split("60(").collect::<Vec<&str>>();
-    let parts1_last = parts1.last().unwrap();
-    let parts2 = parts1_last.split(")").collect::<Vec<&str>>();
-    let parts2_first = parts2.first().unwrap();
-    println!("{}", parts2_first);
-    // VCP 60 active value
-
-    let code = vcp60::get_vcp_60_code(prim);
-    print!("Active VCP 60 feature code:       {}", code);
-
-    // let vcp_value = monitors[0]
-    //     .get_vcp_feature(0x60)
-    //     .expect("Error: Could not determine VCP 60 feature.");
-    // let code = vcp_value.value();
-    // println!("{:02X}", code);
-    return ExitCode::SUCCESS;
-}
-// Get
-if params_count == 2 && params[1] == "/get" {
-    let value = monitors[0].get_vcp_feature(0x60).unwrap();
-    let code = value.value();
-    println!("Actual active VCP 60 feature code is: {:02X}", code);
-    return ExitCode::SUCCESS;
-}
-// Set
-if params_count > 2 && params[1] == "/set" {
-    if params_count < 3 {
-        println!("Error: Missing code value of /set parameter.");
-        println!();
-        print::show_usage_error();
-        return ExitCode::FAILURE;
-    }
-    if params_count == 3 {
-        let code_string = &params[2];
-        let code = code_string.parse::<u16>().unwrap();
-        monitors[0].set_vcp_feature(0x60, code).unwrap();
-        return ExitCode::SUCCESS;
-    }
-}
-println!("Error: Incorrect parameters.");
-print::show_usage();
-return ExitCode::FAILURE;
- */
